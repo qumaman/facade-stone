6c580f4a14fe344f6664913fb2ac2456e7e37ac5
Amanzhol JobComp <qum.aman@gmail.com>
2019-09-06 12:09:12 +0600

    category_id

--- resources/views/admin/stamp/list.blade.php
+++ resources/views/admin/stamp/list.blade.php
@@ -16,6 +16,7 @@
                         <td>#</td>
                         <td>Наименование</td>
                         <td>Артикул</td>
+                        <td>Категория</td>
                         <td>Описание</td>
                         <td><a href="/admin/stamps/create" class="btn btn-success">Добавить</a></td>
                     </tr>
@@ -26,6 +27,7 @@
                         <td>{{ $stamp->id }}</td>
                         <td>{{ $stamp->title }}</td>
                         <td>{{ $stamp->article }}</td>
+                        <td>{{ $stamp->subcategory_id }}</td>
                         <td>{{ $stamp->description }}</td>
                         <td>
                             <a href="{{ route('stamps.edit',$stamp->id)}}" class="btn btn-primary">Редактировать</a>
