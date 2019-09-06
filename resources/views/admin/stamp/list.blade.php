c0ab80c63dc70e8bb821936acb7bac7c05af3b11
Amanzhol JobComp <qum.aman@gmail.com>
2019-09-06 12:10:26 +0600

    panel

--- resources/views/admin/stamp/list.blade.php
+++ resources/views/admin/stamp/list.blade.php
@@ -8,34 +8,34 @@
 
 @section('content')
     <p></p>
+<div class="panel panel-default">
     <div class="col-md-12">
-        <div class="panel panel-default">
-            <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <td>#</td>
-                        <td>Наименование</td>
-                        <td>Артикул</td>
-                        <td>Категория</td>
-                        <td>Описание</td>
-                        <td><a href="/admin/stamps/create" class="btn btn-success">Добавить</a></td>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach($stamps as $stamp)
-                    <tr>
-                        <td>{{ $stamp->id }}</td>
-                        <td>{{ $stamp->title }}</td>
-                        <td>{{ $stamp->article }}</td>
-                        <td>{{ $stamp->subcategory_id }}</td>
-                        <td>{{ $stamp->description }}</td>
-                        <td>
-                            <a href="{{ route('stamps.edit',$stamp->id)}}" class="btn btn-primary">Редактировать</a>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <td>#</td>
+                    <td>Наименование</td>
+                    <td>Артикул</td>
+                    <td>Категория</td>
+                    <td>Описание</td>
+                    <td><a href="/admin/stamps/create" class="btn btn-success">Добавить</a></td>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($stamps as $stamp)
+                <tr>
+                    <td>{{ $stamp->id }}</td>
+                    <td>{{ $stamp->title }}</td>
+                    <td>{{ $stamp->article }}</td>
+                    <td>{{ $stamp->subcategory_id }}</td>
+                    <td>{{ $stamp->description }}</td>
+                    <td>
+                        <a href="{{ route('stamps.edit',$stamp->id)}}" class="btn btn-primary">Редактировать</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
+</div>
 @stop
